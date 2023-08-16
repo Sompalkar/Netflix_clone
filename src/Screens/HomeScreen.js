139ed405894a575a900ff5c422343a730f812ac7
Som Palkar <132274403+Sompalkar@users.noreply.github.com>
2023-08-16 13:02:12 +0530

    Updated HomeScreen.js

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -20,7 +20,7 @@ function HomeScreen() {
             <Row title="Horror Movies " fetchUrl={requests.fetchHorrorMovies} />
             <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
             <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-
+    
 
 
 
